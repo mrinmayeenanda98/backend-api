e826ec6fc3c38e23e002e7b90203d6cc54d91f88
mrinmayeenanda98 <78336918+mrinmayeenanda98@users.noreply.github.com>
2021-12-04 17:49:51 +0530

    push code final

---
+++ WebApplication5/Controllers/CourseController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication5.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CourseController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query = @"
+                    select CourseId,CourseName from
+                    Course
+                    ";
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["StudentAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+
+
+        }
+
+        public string Post(Course course)
+        {
+            try
+            {
+                string query = @"
+                    insert into Course values
+                    ('" + course.CourseName + @"')
+                    ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["StudentAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added Successfully!!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Add!!";
+            }
+        }
+
+
+        public string Put(Course course)
+        {
+            try
+            {
+                string query = @"
+                    update Course set CourseName=
+                    '" + course.CourseName + @"'
+                    where CourseId=" + course.CourseId + @"
+                    ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["StudentAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Updated Successfully!!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Update!!";
+            }
+        }
+
+
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"
+                    delete from Course 
+                    where CourseId=" + id + @"
+                    ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["StudentAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Deleted Successfully!!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Delete!!";
+            }
+        }
+
+
+
+    }
+}
---
+++ WebApplication5/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+
+
+    }
+}
