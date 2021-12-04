99461bbe6bd3d3ef59477a07fcd0087bda9850a4
mrinmayeenanda98 <78336918+mrinmayeenanda98@users.noreply.github.com>
2021-12-04 17:46:24 +0530

    adding backend source

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication5.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query = @"
+                    select StudentId,StudentName,Course,
+                    convert(varchar(10),DateOfJoining,120) as DateOfJoining,
+                    PhotoFileName
+                    from
+                    Student
+                    ";
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["StudentAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+
+
+        }
+
+        public string Post(Student student)
+        {
+            try
+            {
+                string query = @"
+                    insert into Student values
+                    (
+                    '" + student.StudentName + @"'
+                    ,'" + student.Course + @"'
+                    ,'" + student.DateOfJoining + @"'
+                    ,'" + student.PhotoFileName + @"'
+                    )
+                    ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["StudentAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added Successfully!!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Add!!";
+            }
+        }
+
+
+        public string Put(Student student)
+        {
+            try
+            {
+                string query = @"
+                    update Student set 
+                    StudentName='" + student.StudentName + @"'
+                    ,Course='" + student.Course + @"'
+                    ,DateOfJoining='" + student.DateOfJoining + @"'
+                    ,PhotoFileName='" + student.PhotoFileName + @"'
+                    where StudentId=" + student.StudentId + @"
+                    ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["StudentAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Updated Successfully!!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Update!!";
+            }
+        }
+
+
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"
+                    delete from Student 
+                    where StudentId=" + id + @"
+                    ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["StudentAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Deleted Successfully!!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Delete!!";
+            }
+        }
+
+        [Route("api/Student/GetAllCourseNames")]
+        [HttpGet]
+        public HttpResponseMessage GetAllCourseNames()
+        {
+            string query = @"
+                    select CourseName from Course";
+
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["StudentAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        [Route("api/Student/SaveFile")]
+        public string SaveFile()
+        {
+            try
+            {
+                var httpRequest = HttpContext.Current.Request;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = HttpContext.Current.Server.MapPath("~/Photos/" + filename);
+
+                postedFile.SaveAs(physicalPath);
+
+                return filename;
+            }
+            catch (Exception)
+            {
+
+                return "anonymous.png";
+            }
+        }
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Course { get; set; }
+        public string DateOfJoining { get; set; }
+        public string PhotoFileName { get; set; }
+
+    }
+}
